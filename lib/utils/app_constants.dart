3b678add391d1dd7f91e2f69fac0bc9177a1b854
Muhammad Hassan <immaharhasaan@gmail.com>
2025-09-15 16:13:38 +0500

    Update

---
+++ lib/utils/app_constants.dart
@@ -0,0 +1,5 @@
+class AppConstants {
+  static const String baseUrl = "https://api.deepai.org/api";
+  // DEMO key provided by you (do NOT ship like this for production)
+  static const String apiKey = "06be970e-afa0-4150-a186-eda5b221334c";
+}
