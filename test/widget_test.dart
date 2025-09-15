7b93b0bc697496d8b7d693f15e420d315741d3b1
Muhammad Hassan <immaharhasaan@gmail.com>
2025-09-15 16:23:58 +0500

    Update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_application_1/main.dart';
 import 'package:toonx/main.dart';
 
 void main() {
